3c3c37bd1606d8dd86e7892ad1c658c499593cda
Nikita Bragin <nikita-bragin@mail.ru>
2015-07-22 20:22:52 +0300

    update

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,64 +1,45 @@
 'use strict';
 
-var DEBUG = window.adbYtDebug || false;
+$(function () {
+    var $ads = $('.ad-container, .ad-div, .ad-container-single-media-element-annotations, .html5-ad-progress-list, .google_companion_ad_div');
 
-var adbYtLog = function(msg) {
-	if (console && DEBUG) {
-		console.warn(msg);
-	}
-};
+    function skipVideoAd() {
+        if (document.getElementsByClassName('videoAdUi').length > 0) {
+            document.getElementsByClassName('video-stream html5-main-video')[0].src = '';
+        }
+    }
 
-var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-var isChrome = !!window.chrome && !isOpera;
-var player = document.querySelector('#player');
+    function hideAds() {
+        $ads.hide();
+        skipVideoAd();
+    }
 
-function skipVideoAd() {
+    function showAds() {
+        $ads.show();
+    }
 
-	if (document.getElementsByClassName('videoAdUi').length > 0) {
-		adbYtLog('skiping video ad');
-		document.getElementsByClassName('video-stream html5-main-video')[0].src = '';
-	}
-}
+    function clearAds() {
+        chrome.storage.sync.get("youtubePluginEnable", function (items) {
+            if (items.youtubePluginEnable) {
+                hideAds();
+            }
+        });
+    }
 
-function hideOverlayAd() {
-	var overlayAdContainer, buf1, buf2;
-	buf1= [].slice.call(document.getElementsByClassName('ad-container'));
-	buf2 = buf1.concat([].slice.call(document.getElementsByClassName('ad-div')));
-	buf1 = buf2.concat([].slice.call(document.getElementsByClassName('ad-container-single-media-element-annotations')));
-	overlayAdContainer = buf1.concat(document.getElementById('google_companion_ad_div'));
-	for (var i=0; i<overlayAdContainer.length; i++)
-	{	
-		if (overlayAdContainer[i] && overlayAdContainer[i].style.display !== 'none') {
-			adbYtLog('hide overlay ad');
-			overlayAdContainer[i].style.display = 'none';
-		}
-	}
-}
+    chrome.extension.onMessage.addListener(function (message) {
+        switch (message.type) {
+            case "enableYouTubePlugin":
+                chrome.storage.sync.set({"youtubePluginEnable": true});
+                hideAds();
+                break;
+            case "disableYouTubePlugin":
+                chrome.storage.sync.set({"youtubePluginEnable": false});
+                showAds();
+                break;
+        }
+    });
 
-function clearAds() {
-	chrome.storage.sync.get("youtubePluginEnable", function(items){
-		var enable = items.youtubePluginEnable;
-		if(enable)
-		{
-			skipVideoAd();
-			hideOverlayAd();
-		}
-	});
-}
-
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    switch(message.type) {
-        case "enableYouTubePlugin":
-			chrome.storage.sync.set({ "youtubePluginEnable": true });
-			clearAds();
-        break;
-		case "disableYouTubePlugin":
-			chrome.storage.sync.set({ "youtubePluginEnable": false });
-        break;
+    function onYouTubeIframeAPIReady() {
+        clearAds();
     }
 });
-
-window.setInterval(
-	function(){
-		clearAds();
-	}, 300);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,72 +1,47 @@
 'use strict';
 
-function reloadPage(){
-	chrome.tabs.getSelected(null, function(tab) {
-		var code = 'window.location.reload();';
-		chrome.tabs.executeScript(tab.id, {code: code});
-	})
-}
+$(function () {
+    var $hideAds = $('.hide-ads');
 
-function enable(e) {
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(
-      tab.id, 
-      {type: "enableYouTubePlugin"}, 
-      function(response) {
-      }
-    );
-  });
-	var enableElem = document.getElementById("enable"),
-	disableElem = document.getElementById("disable");
-	addClass(enableElem, "active");
-	removeClass(disableElem, "active");
-	reloadPage();
-	window.close();
-}
 
-function disable(e) {
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(
-      tab.id, 
-      {type: "disableYouTubePlugin"}, 
-      function(response) {
-      }
-    );
-  });
-  
-	var enableElem = document.getElementById("enable"),
-	disableElem = document.getElementById("disable");
-	addClass(disableElem, "active");
-	removeClass(enableElem, "active");
-	reloadPage();
-	window.close();
-}
+    function activateEnableTab() {
+        $hideAds.addClass('active');
+    }
 
-function addClass(o, c){
-    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
-    if (re.test(o.className)) return
-    o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
-}
- 
-function removeClass(o, c){
-    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
-    o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "")
-}
+    function deactivateEnableTab() {
+        $hideAds.removeClass('active');
+    }
 
-window.onload = function() {
-	var enableElem = document.getElementById("enable"),
-	disableElem = document.getElementById("disable");
-	chrome.storage.sync.get("youtubePluginEnable", function(items){
-		if(items.youtubePluginEnable)
-		{
-			addClass(enableElem, "active");
-			removeClass(disableElem, "active");
-		}
-		else{
-			addClass(disableElem, "active");
-			removeClass(enableElem, "active");
-		}
-	})
-	enableElem.addEventListener("click", enable);
-	disableElem.addEventListener("click", disable);
-}
+    function enable() {
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.sendMessage(tab.id, {type: 'enableYouTubePlugin'});
+            activateEnableTab();
+        });
+    }
+
+    function disable() {
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.sendMessage(tab.id, {type: 'disableYouTubePlugin'});
+            deactivateEnableTab();
+        });
+    }
+
+    chrome.storage.sync.get('youtubePluginEnable', function (items) {
+        if (items.youtubePluginEnable) {
+            activateEnableTab();
+        } else {
+            deactivateEnableTab();
+        }
+    });
+
+    $hideAds.on('click', function () {
+        var $this = $(this);
+
+        if ($this.hasClass('active')) {
+            disable();
+            return;
+        }
+
+        enable();
+    });
+});
